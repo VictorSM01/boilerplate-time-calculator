89bf90c3c8d354541d377f3f4a41b8a5a1648082
Victor San <62770220+VictorSM01@users.noreply.github.com>
2022-02-08 15:52:19 +0000

    Documentation

--- time_calculator.py
+++ time_calculator.py
@@ -1,7 +1,9 @@
 def add_time(start, duration, day=None):
     """Add a period of time to an initial moment.
 
-    This function does not import any Python libraries and assumes that the start times are valid times. The minutes in the duration time will be a whole number less than 60, but the hour can be any whole number.
+    This function does not import any Python libraries and assumes that the
+    start times are valid times. The minutes in the duration time will be a
+    whole number less than 60, but the hour can be any whole number.
 
     Parameters
     ----------
@@ -16,71 +18,83 @@ def add_time(start, duration, day=None):
     Returns
     -------
     new_time : str
-        time resultang from adding 'duration' to 'start' in the 12-hour clock format.
-        If the result will be the next day, it should show (next day) after the time. If the result will be more than one day later, it should show (n days later) after the time, where "n" is the number of days later.
-        If the function is given the optional starting day of the week parameter, then the output should display the day of the week of the result. The day of the week in the output should appear after the time and before the number of days later.
+        time resultang from adding 'duration' to 'start' in the 12-hour
+        clock format.
+        If the result will be the next day, it should show (next day) after
+        the time. If the result will be more than one day later, it should
+        show (n days later) after the time, where "n" is the number of days
+        later.
+        If the function is given the optional starting day of the week
+        parameter, then the output should display the day of the week of the
+        result. The day of the week in the output should appear after the
+        time and before the number of days later.
     """
 
-    
-    start_time, meridiem = start.split(' ')
-    start_hour, start_minute = start_time.split(':')
-    delta_hour, delta_minute = duration.split(':')
-    
+    # Split data from inputs
+    start_time, meridiem = start.split(" ")
+    start_hour, start_minute = start_time.split(":")
+    delta_hour, delta_minute = duration.split(":")
+    # Convert start hour to 24 hs format
     start_hour = int(start_hour)
-    if meridiem == 'PM':
+    if meridiem == "PM":
         start_hour += 12
-        meridiem = 'AM'
+        meridiem = "AM"
+    # Cast data to integers
     start_minute = int(start_minute)
     delta_hour = int(delta_hour)
     delta_minute = int(delta_minute)
 
+    # Calculate auxiliary quantities
     addition_minutes = start_minute + delta_minute
-
     final_minutes = addition_minutes % 60
-    
     final_hours = start_hour + delta_hour + addition_minutes // 60
-
     extra_days = final_hours // 24
-
     final_hour = final_hours % 12
-
-    if final_hour == 0:
-        final_hour=12
-
     change_meridiem = (final_hours % 24) // 12
 
+    # Correct 0 hour to 12
+    if final_hour == 0:
+        final_hour = 12
+
+    # Perform change of meridiem
     if change_meridiem == 0:
-        final_meridiem = 'AM'
+        final_meridiem = "AM"
     else:
-        if meridiem == 'AM':
-            final_meridiem = 'PM'
+        if meridiem == "AM":
+            final_meridiem = "PM"
         else:
-            final_meridiem = 'AM'
-    
-    if day == None:
-        annotation = ''
+            final_meridiem = "AM"
+
+    # Construct final and conditiona annotaions
+    if day is None:
+        annotation = ""
     else:
         day = day.capitalize()
-        week_encoding ={
-            'Monday': 0,
-            'Tuesday': 1,
-            'Wednesday': 2,
-            'Thursday': 3,
-            'Friday': 4,
-            'Saturday': 5,
-            'Sunday': 6
-            }
-        week_decoding = {value: key for key,value in week_encoding.items()}
+        # Dictionary containing encoding for days of the week
+        week_encoding = {
+            "Monday": 0,
+            "Tuesday": 1,
+            "Wednesday": 2,
+            "Thursday": 3,
+            "Friday": 4,
+            "Saturday": 5,
+            "Sunday": 6,
+        }
+        # Dictionary with decoding key
+        week_decoding = {value: key for key, value in week_encoding.items()}
+        # Calculate final day of the week
         first_day = week_encoding[day]
         last_day = (first_day + extra_days) % 7
         last_day = week_decoding[last_day]
-        annotation =f', {last_day}'
+        annotation = f", {last_day}"
 
     if extra_days == 1:
-        annotation += ' (next day)'
+        annotation += " (next day)"
     elif extra_days > 1:
-        annotation += f' ({extra_days} days later)'
-    
-    new_time = f'{final_hour}:{final_minutes:02d} {final_meridiem:s}{annotation}'
+        annotation += f" ({extra_days} days later)"
+
+    # Construct output
+    new_hour = f"{final_hour}:{final_minutes:02d}"
+    new_time = f"{new_hour} {final_meridiem:s}{annotation}"
 
     return new_time
